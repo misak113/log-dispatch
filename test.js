deeb509bfbf575cb21dffb868d38f81c86aa95dc
Michael Zabka <zabka.michael@seznam.cz>
2013-04-16 19:42:10 +0200

    test

---
+++ test.js
@@ -0,0 +1,11 @@
+
+
+var l = require('./log-dispatch');
+
+l.debug('some text'); // l.d('some text');
+
+l.log('some text');
+
+l.warn('some text'); // l.warning('some text');
+
+l.error('some text');
